6f7e7d86ac7dcf38e586aa97b95ece30f569b6c2
Piotr Walacik <piotr.walacik@hotmail.com>
2021-12-07 22:16:56 +0100

    add testing validator

---
+++ tests/Travels.Infrastructure.Tests/QueriesValidatorTests/GetByIdTravelsQuerieValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation.TestHelper;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travels.Core.Queries;
+using Travels.Infrastructure.QueriesValidator;
+
+namespace Travels.Infrastructure.Tests.QueriesValidatorTests
+{
+    internal class GetByIdTravelsQuerieValidatorTests
+    {
+            [Test]
+            public void Should_no_have_error_when_id_is_specified()
+            {
+                //Arange
+                var validator = new GetByIdTravelsQuerieValidator();
+                var model = new GetByIdTravelsQuerie() { Id = 1 };
+
+                //Act
+                var result = validator.TestValidate(model);
+
+                //Asert
+                result.ShouldNotHaveValidationErrorFor(model => model.Id);
+            }
+
+            [Test]
+            public void Should_have_error_when_id_is_Empty()
+            {
+                //Arange
+                var validator = new GetByIdTravelsQuerieValidator();
+                var model = new GetByIdTravelsQuerie();
+
+                //Act
+                var result = validator.TestValidate(model);
+
+                //Asert
+                result.ShouldHaveValidationErrorFor(model => model.Id);
+            }
+    }
+}
